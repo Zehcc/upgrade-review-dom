5696bfb77b1e242837ffa18cf0a98baef4b5e20c
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-03-06 17:58:53 +0100

    chore: Iteration 1 casi completa, me falta el ultimo punto

--- Iteration1.js
+++ Iteration1.js
@@ -3,12 +3,108 @@ dinámicamente en el html que imprima cada uno de los paises. */
 
 const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
 
+window.onload = () => {
+    createCountries();
+    createCars();
+    createCountriesTwo();
+    createButton();
+};
 const createCountries = () =>{
     let countriesList = document.createElement('ul')
     countries.forEach(country => {
-        countriesList.appendChild(country);
-        return countriesList
-    });    
+        let countriesItem = document.createElement('li');
+        countriesItem.textContent = country;
+        countriesList.appendChild(countriesItem);
+    }); 
+    document.body.appendChild(countriesList);   
 }
 
+/* 1.2 Elimina el elemento que tenga la clase .fn-remove-me. */
 
+let removeME = document.querySelector('.fn-remove-me')
+
+document.body.removeChild(removeME)
+
+/* 1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
+en el div de html con el atributo data-function="printHere". */
+
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const createCars = () => {
+    let carsList = document.createElement('ul');
+    let listDiv = document.querySelector('[data-function="printHere"]');
+    cars.forEach(car => {
+        let carItem = document.createElement('li')
+        carItem.textContent = car;
+        carsList.appendChild(carItem);
+    });
+    listDiv.appendChild(carsList)
+}
+
+/* 1.4 Crea dinamicamente en el html una lista de div que contenga un elemento 
+h4 para el titulo y otro elemento img para la imagen. */
+
+const countries2 = [
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+];
+
+const createCountriesTwo = () => {
+    let listContainer = document.createElement('div');
+    listContainer.className = 'listContainer';
+    let countriesListTwo = document.createElement('ul');
+    countriesListTwo.className = 'countriesList'
+    countries2.forEach(country => {
+        let countryDiv = document.createElement('div')
+        countryDiv.className = 'countryItem'
+        let listTitle = document.createElement('h4');
+        listTitle.className = 'listTitle';
+        let listImg = document.createElement('img');
+        listImg.className = 'listImg';
+        listTitle.textContent = country.title;
+        listImg.src = country.imgUrl;
+        countriesListTwo.appendChild(countryDiv)
+        countryDiv.appendChild(listTitle);
+        countryDiv.appendChild(listImg);
+    });
+    
+    listContainer.appendChild(countriesListTwo);
+    document.body.appendChild(listContainer)
+    
+}
+
+/* 1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último 
+elemento de la lista. */
+
+let createButton = () => {
+    let removeButton = document.createElement('button')
+    removeButton.textContent = 'Click para eliminar el ultimo pais de la lista'
+    removeButton.addEventListener('click', clickRemove)
+    document.body.appendChild(removeButton)
+}
+
+let clickRemove = () => {
+    let removeItem = document.querySelector('.countriesList')
+    removeItem.removeChild(removeItem.lastChild)
+}
+
+/* 1.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los 
+elementos de las listas que elimine ese mismo elemento del html. */
+
+/* let createButton2 = () => {
+    let countryDivs = document.getElementsByClassName('countryItem')
+    let countryDiv = document.querySelector('.countryItem')
+    for (const country of countryDivs) {
+        let removeButton = document.createElement('button')
+        removeButton.textContent = 'Click para eliminar este pais'
+        removeButton.addEventListener('click', clickRemove)
+        countryDiv.appendChild(removeButton)
+    }
+}
+
+let clickRemove2 = () => {
+
+} */
